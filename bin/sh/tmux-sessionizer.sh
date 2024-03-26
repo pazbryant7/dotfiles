e3ae0786f003b597047c8d526d63919271bfdb74
Bryant Paz <plbryant@hotmail.com>
2024-03-25 20:29:09 -0600

    chore: update script tmux sessionizer

--- bin/sh/tmux-sessionizer.sh
+++ bin/sh/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-  selected=$(fd --min-depth 1 --max-depth 1 --type d . ~/Documents/github ~/Downloads | fzf)
+  selected=$(fd --min-depth 1 --max-depth 1 --type d . ~/Documents/github ~/Documents/github/examples ~/Downloads | fzf)
 fi
 
 if [[ -z $selected ]]; then
