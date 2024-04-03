8fae3a52f2c3ebc8b94de4de566d6a30ddaef445
Bryant Paz <plbryant@hotmail.com>
2024-04-03 15:28:42 -0600

    chore: update tmuk sessionizer

--- bin/sh/tmux-sessionizer.sh
+++ bin/sh/tmux-sessionizer.sh
@@ -1,36 +1,25 @@
 #!/bin/sh
 
-if [[ $# -eq 2 ]]; then
-    selected_dir=$1
-    selected_file=$2
-elif [[ $# -eq 1 ]]; then
-    selected_dir=$1
+if [[ $# -eq 1 ]]; then
+    selected=$1
 else
-    selected_dir=$(fd --min-depth 1 --max-depth 1 --type d . ~/Documents/github ~/Documents/github/royal ~/Documents/github/examples ~/Downloads | fzf)
+    selected_dir=$(fd --min-depth 1 --max-depth 1 --type d . ~/Documents/github ~/Documents/github/royal /Documents/github/examples ~/Downloads | fzf)
 fi
 
-if [[ -z $selected_dir ]]; then
+if [[ -z $selected ]]; then
     exit 0
 fi
 
-selected_name=$(basename "$selected_dir" | tr . _)
+selected_name=$(basename "$selected" | tr . _)
 tmux_running=$(pgrep tmux)
 
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    if [[ -n $selected_file ]]; then
-        tmux new-session -s $selected_name "nvim $selected_file"
-    else
-        tmux new-session -s $selected_name -c $selected_dir
-    fi
+    tmux new-session -s $selected_name -c $selected
     exit 0
 fi
 
 if ! tmux has-session -t=$selected_name 2> /dev/null; then
-    if [[ -n $selected_file ]]; then
-        tmux new-session -ds $selected_name "nvim $selected_file"
-    else
-        tmux new-session -ds $selected_name -c $selected_dir
-    fi
+    tmux new-session -ds $selected_name -c $selected
 fi
 
 tmux switch-client -t $selected_name
