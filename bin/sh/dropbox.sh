f9a18e53116c2118a60140b62423b2fddc50b2df
Bryant Paz <plbryant@hotmail.com>
2024-04-16 12:32:15 -0600

    chore: dropbox sync
    - add dropbox sync scrip with rclone
    - add keybinds to sync dropbox and mega

---
+++ bin/sh/dropbox.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -x
+
+# Set the paths
+local_folder="$HOME/dropbox"
+remote_folder="dropbox:DropboxSync"
+
+# Display a dialog window to select the action
+selected_option=$(echo -e "Sync from local to dropbox\nSync from dropbox to local\nCancel" | rofi -dmenu -i -p "Select Action:")
+
+case "$selected_option" in
+"Sync from local to dropbox")
+	# Check if there are changes
+	if rclone check "$local_folder" "$remote_folder" &>/dev/null; then
+		# No changes, show notification and exit
+		dunstify -u low -t 3000 "DropboxSync" "No changes to sync."
+		exit 0
+	fi
+
+	# Proceed with sync
+	rclone sync "$local_folder" "$remote_folder"
+	dunstify -u low -t 3000 "DropboxSync" "Sync from local to dropbox completed successfully."
+	;;
+"Sync from dropbox to local")
+	# Check if there are changes
+	if rclone check "$remote_folder" "$local_folder" &>/dev/null; then
+		# No changes, show notification and exit
+		dunstify -u low -t 3000 "DropboxSync" "No changes to sync."
+		exit 0
+	fi
+
+	# Proceed with sync
+	rclone sync "$remote_folder" "$local_folder"
+	dunstify -u low -t 3000 "DropboxSync" "Sync from dropbox to local completed successfully."
+	;;
+"Cancel")
+	# User selected cancel, exit
+	dunstify -u low -t 3000 "DropboxSync" "Action canceled by the user."
+	exit 0
+	;;
+esac
