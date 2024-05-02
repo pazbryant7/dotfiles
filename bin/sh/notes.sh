551b67c84a74e8d9746bf6beaff7066ea3df3fad
Bryant Paz <plbryant@hotmail.com>
2024-05-01 21:24:36 -0600

    chore: update notes sync

--- bin/sh/notes.sh
+++ bin/sh/notes.sh
@@ -11,32 +11,16 @@ selected_option=$(echo -e "Sync from local to remote\nSync from remote to local\
 
 case "$selected_option" in
 "Sync from local to remote")
-	# Check if there are changes
-	if rclone check "$local_folder" "$remote_folder" &>/dev/null; then
-		# No changes, show notification and exit
-		dunstify "Notes" "No changes to sync."
-		exit 0
-	fi
-
-	# Proceed with sync
-	rclone sync "$local_folder" "$remote_folder" --exclude=".git/**" --delete-before
-	dunstify "Notes" "Sync from local to remote completed successfully."
+	rclone sync "$local_folder" "$remote_folder" --exclude=".gitignore" --exclude=".git/**" --exclude=".prettierrc" --delete-before
+	dunstify "Notes" "Sync successfully."
 	;;
 "Sync from remote to local")
-	# Check if there are changes
-	if rclone check "$remote_folder" "$local_folder" &>/dev/null; then
-		# No changes, show notification and exit
-		dunstify "Notes" "No changes to sync."
-		exit 0
-	fi
-
-	# Proceed with sync
-	rclone sync "$remote_folder" "$local_folder" --exclude=".git/**" --delete-before
-	dunstify "Notes" "Sync from remote to local completed successfully."
+	rclone sync "$remote_folder" "$local_folder" --exclude=".gitignore" --exclude=".git/**" --exclude=".prettierrc" --delete-before
+	dunstify "Notes" "Sync successfully."
 	;;
 "Cancel")
 	# User selected cancel, exit
-	dunstify "Notes" "Action canceled by the user."
+	dunstify "Notes" "Action canceled."
 	exit 0
 	;;
 esac
