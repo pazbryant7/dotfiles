adfebd5c6e538b01e7bf13f5e1d0e5d3a643b243
Bryant Paz <pazbryant7@outlook.com>
2024-05-06 02:03:26 -0600

    chore: update notes script file

--- bin/sh/notes.sh
+++ bin/sh/notes.sh
@@ -2,25 +2,57 @@
 
 set -x
 
-# Set the paths
 local_folder="$HOME/Documents/github/notes"
 remote_folder="mega:phone/notes"
 
-# Display a dialog window to select the action
-selected_option=$(echo -e "Sync from local to remote\nSync from remote to local\nCancel" | rofi -dmenu -i -p "Select Action:")
+sync_options='--exclude=".gitignore" \
+--exclude=".git/**" \
+--exclude=".prettierrc" \
+--delete-before'
 
-case "$selected_option" in
-"Sync from local to remote")
-	rclone sync "$local_folder" "$remote_folder" --exclude=".gitignore" --exclude=".git/**" --exclude=".prettierrc" --delete-before
-	dunstify "Notes" "Sync successfully."
-	;;
-"Sync from remote to local")
-	rclone sync "$remote_folder" "$local_folder" --exclude=".gitignore" --exclude=".git/**" --exclude=".prettierrc" --delete-before
-	dunstify "Notes" "Sync successfully."
-	;;
-"Cancel")
-	# User selected cancel, exit
-	dunstify "Notes" "Action canceled."
-	exit 0
-	;;
-esac
+sync_notes_from() {
+	from=$1
+	to=$2
+	options=$3
+
+	rclone sync "$from" "$to" "$options"
+
+	if [ "$?" -eq 0 ]; then
+		return 0 # Success
+	else
+		return 1 # Failure
+	fi
+}
+
+send_notification() {
+	message=$1
+	dunstify "Notes" "$message"
+}
+
+sync() {
+	case "$1" in
+	"Sync from local to remote")
+		sync_notes_from "$local_folder" "$remote_folder" "$sync_options" &&
+			send_notification "Sync successfully." ||
+			send_notification "Sync failed."
+		;;
+	"Sync from remote to local")
+		sync_notes_from "$remote_folder" "$local_folder" "--delete-before" &&
+			send_notification "Sync successfully." ||
+			send_notification "Sync failed."
+		;;
+	"Cancel")
+		# User selected cancel, exit
+		send_notification "Action canceled."
+		;;
+	esac
+}
+
+if (("$1" == "hook")); then
+	selected_option="Sync from local to remote"
+else
+	selected_option=$(echo -e "Sync from local to remote\nSync from remote to local\nCancel" |
+		rofi -dmenu -i -p "Select Action:")
+fi
+
+sync "$selected_option"
