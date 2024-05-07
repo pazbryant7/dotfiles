c81d932f486833a94b264abce898972f4b7c4a2a
Bryant Paz <pazbryant7@outlook.com>
2024-05-07 00:09:01 -0600

    chore: disable noti to bringtess script

--- bin/sh/set-brightness.sh
+++ bin/sh/set-brightness.sh
@@ -3,18 +3,20 @@
 brightness=$(rofi -dmenu -p "Enter brightness (0 to 1.0):" -lines 1)
 
 # Check if the input is '2' and set brightness to 0.5 by default
-if [[ "$brightness" == "2" ]]; then
-    brightness="0.5"
+if [ "$brightness" = "2" ]; then
+	brightness="0.5"
 fi
 
-if [[ "$brightness" == "3" ]]; then
-    brightness="0.4"
+if [ "$brightness" = "3" ]; then
+	brightness="0.4"
 fi
 
-if [[ "$brightness" == "4" ]]; then
-    brightness="0.3"
+if [ "$brightness" = "4" ]; then
+	brightness="0.3"
 fi
 
-if [[ $brightness =~ ^[0-1](\.[0-9]+)?$ ]]; then
-    xrandr --output HDMI-2 --brightness $brightness
+if [ "$brightness" = "5" ]; then
+	brightness="0.2"
 fi
+
+xrandr --output HDMI-2 --brightness "$brightness"
