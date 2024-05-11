66a09c3627689d4c6a3f1c26d22cbaf0d62178e0
Bryant Paz <pazbryant7@outlook.com>
2024-05-11 02:49:25 -0600

    chore: update lua script

--- bin/lua/set-brightness.lua
+++ bin/lua/set-brightness.lua
@@ -1,9 +1,9 @@
-function read_input(prompt)
+local function read_input(prompt)
 	io.write(prompt)
 	return io.read()
 end
 
-function set_brightness(brightness)
+local function set_brightness(brightness)
 	if brightness == "2" then
 		return "0.5"
 	elseif brightness == "3" then
@@ -17,9 +17,11 @@ function set_brightness(brightness)
 	end
 end
 
-function main()
+local function main()
 	local brightness = read_input("Enter brightness (0 to 1.0):")
 	brightness = set_brightness(brightness)
 
 	os.execute("xrandr --output HDMI-2 --brightness " .. brightness)
 end
+
+main()
