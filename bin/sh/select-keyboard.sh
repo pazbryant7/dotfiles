3e6e6494ab9de2ad47f8efa662b53a4a203ea9d2
Bryant Paz <pazbryant7@outlook.com>
2024-06-23 00:44:10 -0600

    chore: window manager, polybar, keybinds and scripts
    - Update keyboard switch keybind
    - Add keyboard switch script
    - Update window manager (bspmw) options
    - Update polybar options

---
+++ bin/sh/select-keyboard.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+set -x
+set -e
+
+CURRENT_KEYBOARD="$HOME/.current_keyboard"
+
+DEVORAK_LAYOUT="$HOME/.Xmodmap"
+SPANISH_LAYOUT="$HOME/.XmodmapIntl"
+
+read_state() {
+	bat "$CURRENT_KEYBOARD"
+}
+
+set_keyboard_layout() {
+	xmodmap "$1"
+}
+
+send_notification() {
+	dunstify "$1 layout"
+}
+
+toggle_keyboard() {
+	state="$1"
+
+	devorak="Devorak"
+	spanish="Spanish"
+
+	if [ "$state" = "$devorak" ]; then
+		set_keyboard_layout "$SPANISH_LAYOUT"
+		echo "$spanish" >"$CURRENT_KEYBOARD"
+	else
+		set_keyboard_layout "$DEVORAK_LAYOUT"
+		echo "$devorak" >"$CURRENT_KEYBOARD"
+	fi
+}
+
+CURRENT_STATE=$(read_state)
+toggle_keyboard "$CURRENT_STATE"
+
+CURRENT_STATE=$(read_state)
+send_notification "$CURRENT_STATE"
