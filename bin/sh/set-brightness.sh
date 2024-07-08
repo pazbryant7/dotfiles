4096074de17d0dfc3082488f4dcdc79eabb67cda
Bryant Paz <pazbryant7@outlook.com>
2024-07-08 03:19:12 -0600

    chore: update bightness script

--- bin/sh/set-brightness.sh
+++ bin/sh/set-brightness.sh
@@ -4,22 +4,26 @@ brightness=$(rofi -dmenu -p "Enter brightness (0 to 1.0):" -lines 1)
 
 # Check if the input is '2' and set brightness to 0.5 by default
 if [ "$brightness" = "2" ]; then
-	brightness="0.5"
+	brightness="0.7"
 fi
 
 if [ "$brightness" = "3" ]; then
-	brightness="0.4"
+	brightness="0.5"
 fi
 
 if [ "$brightness" = "4" ]; then
-	brightness="0.3"
+	brightness="0.4"
 fi
 
 if [ "$brightness" = "5" ]; then
-	brightness="0.25"
+	brightness="0.3"
 fi
 
 if [ "$brightness" = "6" ]; then
+	brightness="0.25"
+fi
+
+if [ "$brightness" = "7" ]; then
 	brightness="0.2"
 fi
 
