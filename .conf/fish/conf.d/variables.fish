165c11fa24da9f56a516b6735812b6833fe6fff1
Bryant Paz <pazbryant7@outlook.com>
2024-07-24 00:29:04 -0600

    chore: update color schemes to catppuccin late

--- .conf/fish/conf.d/variables.fish
+++ .conf/fish/conf.d/variables.fish
@@ -19,7 +19,10 @@ set -x CM_LAUNCHER "rofi"
 set -x SXHKD_SHELL "sh"
 
 # FZF
-set -x FZF_DEFAULT_OPTS " \
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
+--color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
+--color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
 --preview-window noborder \
 --height=50%"
 
