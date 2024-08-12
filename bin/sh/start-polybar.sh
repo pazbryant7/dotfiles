440aa97cf98e14c94338189dc3ad15fb276e4443
Bryant Paz <pazbryant@proton.me>
2024-08-11 20:17:26 -0600

    chore: update polybar scrip start

--- bin/sh/start-polybar.sh
+++ bin/sh/start-polybar.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do :; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-	WIRELESS=$(ls /sys/class/net/ | grep ^wl | awk 'NR==1{print $1}') MONITOR=$m polybar --reload mainbar-bspwm &
+  MONITOR=$m polybar --reload mainbar-bspwm &
 done
