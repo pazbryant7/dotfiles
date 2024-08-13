75e8763f11afa130b9d0a068bd24c064dd5b641c
Bryant Paz <pazbryant@proton.me>
2024-08-13 17:33:11 -0600

    chore: BIG UPDATE
    - remove all types of colors
    - use default color schemes

--- .conf/fish/conf.d/abbr.fish
+++ .conf/fish/conf.d/abbr.fish
@@ -1,68 +1,41 @@
-# fihs
-abbr --add cf "vi ~/.config/fish/config.fish"
+# main
+abbr --add :q "exit"
+abbr --add cl "clear"
+abbr --add sudo "sudo "
+abbr --add pass "gopass"
+abbr --add transes "trans en:es"
+abbr --add vi "NVIM_APPNAME 'minimal' nvim"
 
-# zsh
-abbr --add czsh "vi ~/.zshrc"
-
-# general
+# default
 abbr --add sp "systemctl suspend"
-abbr --add weather "wego"
 abbr --add dh "rm ~/.history.db"
 abbr --add sht "shutdown now"
 abbr --add clear "clear; printf '\033[4 q'"
-abbr --add ck "vi ~/.config/sxhkd/sxhkdrc"
-abbr --add cwm "vi ~/.config/bspwm/bspwmrc"
 abbr --add fontcache "sudo fc-cache -f -v"
-abbr --add motivation "mpv ~/mega/personal/pc/videos/biy/dios.mp4"
-abbr --add rel "xrdb merge ~/.Xresources && kill -USR1 (pidof st)"
-abbr --add rma "rm -rf"
-abbr --add leetcode "vi leetcode.nvim"
+abbr --add cristiano "mpv ~/mega/personal/pc/videos/biy/dios.mp4"
+abbr --add rel "xrdb merge ~/.Xresources && kill -USR1 \$(pidof st)"
+abbr --add leetcode "vi leetcode.vi"
 abbr --add ff "fastfetch"
-abbr --add sudoe "sudoedit"
-abbr --add transes "trans en:es"
-abbr --add cheat "cht.sh"
-
-# zoxide
-abbr --add . "cd ../"
-abbr --add .. "cd ../../"
-abbr --add ... "cd ../../../"
-abbr --add .... "cd ../../../../"
 
 # git
 abbr --add gb "git branch"
-abbr --add glg "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
-abbr --add glga "git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Crese' --stat"
+abbr --add glg "git log --graph --pretty '%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --all"
 abbr --add gsb "git status --short --branch"
 abbr --add lzg "lazygit"
 abbr --add gdc "git diff --cached"
 abbr --add gd "git diff"
+abbr --add gco "git checkout"
 abbr --add gaa "git add -A"
 abbr --add gc "git commit"
-abbr -a ggpush 'git push origin (git rev-parse --abbrev-ref HEAD)'
-abbr -a ggpull 'git pull origin (git rev-parse --abbrev-ref HEAD)'
-
-abbr --add git-personal "git config --global user.name 'bryantpaz'; git config --global user.email 'pazbryant7@outlook.com'"
-abbr --add git-work "git config --global user.name 'pazbryant'; git config --global user.email 'bryantpaz7@outlook.com'"
-
-# docker
-abbr --add lzd "lazydocker"
-abbr --add docker-start "sudo systemctl start docker.socket"
-abbr --add docker-stop "sudo systemctl stop docker.socket"
-
-# mongo
-abbr --add mongo-start "sudo systemctl start mongodb.service"
-abbr --add mongo-stop "sudo systemctl stop mongodb.service"
-
-# postgres
-abbr --add postgres-start "sudo systemctl start postgresql.service"
-abbr --add postgres-stop "sudo systemctl stop postgresql.service"
+abbr --add ggpush 'git push origin $(git rev-parse --abbrev-ref HEAD)'
+abbr --add ggpull 'git pull origin $(git rev-parse --abbrev-ref HEAD)'
 
 # tmux
 abbr --add ta "tmux attach"
 abbr --add tc "vi ~/.tmux.conf"
 abbr --add tk "tmux kill-server"
-abbr --add tn "tmux new -s $USER"
+abbr --add tn 'tmux new -s $USER'
 
-# open vpn
-abbr --add openvpnjapan "sudo openvpn /etc/openvpn/client/jp-free-127011.protonvpn.udp.ovpn"
-abbr --add openvpnus "sudo openvpn /etc/openvpn/client/us-free-111014.protonvpn.udp.ovpn"
+# docker
+abbr --add tachidesk 'cd ~/documents/github/tachidesk && docker compose start'
+abbr --add lzd "lazydocker"
--- .conf/fish/conf.d/variables.fish
+++ .conf/fish/conf.d/variables.fish
@@ -1,29 +1,22 @@
 # Environment variables
 set -x EDITOR "nvim"
-set -x VISUAL $gDITOR
+set -x VISUAL $EDITOR
 set -x TERMINAL "st"
 set -x SUDO_EDITOR $EDITOR
-set -x BROWSER "firefox-developer-edition"
+set -x BROWSER "firefox"
 set -x READER "zathura"
 set -x VIDEO "mpv"
 set -x COLORTERM "truecolor"
 set -x OPENER "xdg-open"
 set -x PAGER "less"
 set -x BAT_PAGER $PAGER
-set -x DELTA_PAGER "less -R"
 set -x WM "bspwm"
 set -x MANPAGER "nvim +Man!"
-set -x QT_QPA_PLATFORMTHEME "qt5ct"
-set -x GOPATH "/home/bryant/go/bin"
 set -x CM_LAUNCHER "rofi"
 set -x SXHKD_SHELL "sh"
-set -x CHTSH_QUERY_OPTIONS "style=perldoc"
 
 # FZF
 set -Ux FZF_DEFAULT_OPTS "\
---color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39 \
---color=fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78 \
---color=marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39 \
 --preview-window noborder \
 --height=50%"
 
