a7cf23c07ebda58c33f1602e98948175b124ec2c
Bryant Paz <pazbryant7@outlook.com>
2025-07-20 23:20:09 -0600

    feat(fish): adding a better documentation for fish functions

--- .config/fish/functions/clear_and_redisplay.fish
+++ .config/fish/functions/clear_and_redisplay.fish
@@ -1,3 +1,5 @@
+# NOTE: this function is used in ~/.zshconfig file
+# is attached to ^L keybind
 function clear_and_redisplay
     clear
     commandline -f repaint
--- .config/fish/functions/stow_dots.fish
+++ .config/fish/functions/stow_dots.fish
@@ -17,6 +17,8 @@ function stow_dots --description "Restow dotfiles using GNU Stow from ~/Document
 
     # Run stow with optional extra args
     command stow --target="/home/$USER" --adopt --restow .
+
+		cd - # get back to previous dir
     # the "." at the end it is important, because stow needs to know which files
     # you want to add to the "--target" folder. If not, then no dir nor file will
     # be symlinked
