bacf0112a502260eeaa5c22113a90aca39e7f6bd
Bryant Paz <pazbryant7@outlook.com>
2025-11-07 17:13:07 -0600

    feat(shell): new aliases

--- .config/fish/conf.d/02_aliases.fish
+++ .config/fish/conf.d/02_aliases.fish
@@ -7,13 +7,14 @@ alias cdi='zi'
 alias ls="ls"
 alias :q="exit"
 alias cl="clear"
+alias vi="nvim --noplugin"
 alias xr="nvim ~/.Xresources"
 alias leetcode="nvim leetcode.nvim"
-alias zsha="cd ~/zsh && nvim aliases"
+alias zsha="cd ~/zsh && nvim --noplugin aliases"
 alias c="xclip -selection clipboard"
-alias zshc="cd ~/zsh && nvim zshconfig"
+alias zshc="cd ~/zsh && nvim --noplugin zshconfig"
 alias p="xclip -selection clipboard -o"
-alias fishc="cd ~/.config/fish && nvim config.fish"
+alias fishc="cd ~/.config/fish && nvim --noplugin config.fish"
 alias rex="xrdb -remove && xrdb -merge ~/.Xresources && kill -USR1 \$(pidof st)"
 
 # system
