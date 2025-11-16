85142c1f2647611279dcbb1955d835cbd60fa80a
Bryant Paz <pazbryant7@outlook.com>
2025-11-16 11:20:22 -0600

    feat(shell): update aliases

--- .config/fish/conf.d/02_aliases.fish
+++ .config/fish/conf.d/02_aliases.fish
@@ -4,16 +4,17 @@ alias ls='eza'
 alias cdi='zi'
 
 # main
+
 alias :q="exit"
 alias cl="clear"
 alias vi="nvim --noplugin"
+alias zshc="nvim ~/.zshrc"
 alias xr="nvim ~/.Xresources"
 alias jjh="jj bookmark set main"
 alias leetcode="nvim leetcode.nvim"
 alias c="xclip -selection clipboard"
 alias p="xclip -selection clipboard -o"
 alias zsha="cd ~/zsh && nvim --noplugin aliases"
-alias zshc="cd ~/zsh && nvim --noplugin zshconfig"
 alias fishc="cd ~/.config/fish && nvim --noplugin config.fish"
 alias alac="nvim --noplugin ~/.config/alacritty/alacritty.toml"
 alias rex="xrdb -remove && xrdb -merge ~/.Xresources && kill -USR1 \$(pidof st)"
