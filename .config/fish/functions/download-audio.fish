d16a8be76c1c189e28b32d7047b6626393f02449
Bryant Paz <pazbryant7@outlook.com>
2026-03-26 17:00:23 -0600

    feat(download-audio,youtube-dl): now download as a mp3 optionally

--- .config/fish/functions/download-audio.fish
+++ .config/fish/functions/download-audio.fish
@@ -1,19 +1,41 @@
-function download-audio --description "Download music from youtube"
-    set -l url $argv[1]
+function download-audio
+    set -l url ""
+    set -l mp3 false
     set -l cookie_file "$HOME/Downloads/cookies.txt"
+
+    for arg in $argv
+        switch $arg
+            case -m --mp3
+                set mp3 true
+            case '-*'
+                echo "Error: Unknown flag '$arg'"
+                return 1
+            case '*'
+                set url $arg
+        end
+    end
 
     if test -z "$url"
         echo "Error: No URL provided."
+        echo "Usage: download-audio [--mp3] <url>"
         return 1
     end
 
-    if test ! -f "$cookie_file"
+    if not test -f "$cookie_file"
         echo "Error: cookies.txt not found in ~/Downloads/"
         echo "Please place your cookies.txt file in: $cookie_file"
         return 1
     end
 
-    echo "Downloading best audio quality from $url..."
+    set -l extra_args
+
+    if $mp3
+        echo "Downloading as MP3 from $url..."
+        set extra_args --audio-format mp3
+    else
+        echo "Downloading best audio quality from $url..."
+    end
+
     yt-dlp -f bestaudio \
         -x \
         --verbose \
@@ -22,6 +44,7 @@ function download-audio --description "Download music from youtube"
         --embed-thumbnail \
         --cookies "$cookie_file" \
         --js-runtimes bun:/usr/bin/bun \
-        -o "%(artist)s - %(title)s.%(ext)s" \
+        -o "%(title)s.%(ext)s" \
+        $extra_args \
         "$url"
 end
