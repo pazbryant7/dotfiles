49ef9316c3fc5e5b97c7fc1a3d76a52b155ba6d7
Bryant Paz <pazbryant7@outlook.com>
2026-06-25 16:24:12 -0600

    feat(shell): python as a calculator

--- .config/fish/conf.d/02_aliases.fish
+++ .config/fish/conf.d/02_aliases.fish
@@ -23,6 +23,7 @@ alias zsha="cd ~/zsh && nvim aliases"
 alias fishc="cd ~/.config/fish && nvim config.fish"
 alias alac="nvim ~/.config/alacritty/alacritty.toml"
 alias rex="xrdb -remove && xrdb -merge ~/.Xresources && kill -USR1 \$(pidof st)"
+alias calc="python -i -c 'import math, cmath; from math import *'"
 
 alias opencode="bunx --bun opencode-ai"
 
