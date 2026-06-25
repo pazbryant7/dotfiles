2e59c9a7d21477b50ef794e96718a5229c078737
Bryant Paz <pazbryant7@outlook.com>
2026-06-24 23:39:09 -0600

    feat(shell): clear and redisplay now working

--- .config/fish/functions/clear-and-redisplay.fish
+++ .config/fish/functions/clear-and-redisplay.fish
@@ -1,6 +1,6 @@
 # NOTE: this function is used in ~/.zshconfig file
 # is attached to ^L keybind
 function clear-and-redisplay
-    clear
+    printf '\033[2J\033[3J\033[H'
     commandline -f repaint
 end
